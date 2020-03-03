cf293478f2d54ee4a46184f2b6c3b5aa20e15158
Thomas Ferris <A01211467@ad.bcit.ca>
2020-03-03 12:37:32 -0800

    article and view

--- app/src/main/java/com/tomkf/nooz/MainActivity.kt
+++ app/src/main/java/com/tomkf/nooz/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.tomkf.nooz
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.tomkf.nooz.network.Article
 import com.tomkf.nooz.network.API
 import com.tomkf.nooz.network.ArticleResponse
@@ -17,10 +22,11 @@ import kotlinx.coroutines.launch
 class MainActivity : AppCompatActivity() {
     private val api = NewsAPI()
     private var articles: List<Article> = listOf()
+    private class ArticleViewHolder(view: View): RecyclerView.ViewHolder(view)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.article_list)
+        setContentView(R.layout.activity_main)
 
         // We will use a coroutine to help us simplify the asynchronous code
         loadPopularArticles()
@@ -36,8 +42,20 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun update() {
-//        article_list.adapter = ArticlesAdapter(articles, this)
         article_list.layoutManager = GridLayoutManager(this, 2)
     }
 
-}
+    private class ArticleAdaptor (val recipes: List<Article>, val context: Context): RecyclerView.Adapter<ArticleViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleViewHolder {
+            return ArticleViewHolder(LayoutInflater.from(context).inflate(R.layout.article_list, parent, false))
+        }
+
+        override fun getItemCount(): Int {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun onBindViewHolder(holder: ArticleViewHolder, position: Int) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+    }
